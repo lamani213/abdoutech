6a0bb9e851d3ce58a0f769e1df6b310762942541
lamani213 <46058208+lamani213@users.noreply.github.com>
2018-12-21 22:54:04 +0100

    Create abdoutechfuck.js

---
+++ abdoutechfuck.js
@@ -0,0 +1,37 @@
+var _0x4816=["$(i)['f'](5(){$('#3')['j']('<a 4=\"2://6.7.8\" c=\"d\" e=\"b\" g=\"h:9!0;k:1!0;l:m!0\">\u062A\u0635\u0645\u064A\u0645:\u0639\u0628\u062F\u0648 \u062A\u0643\u0646\u0648\u0644\u0648\u062C\u064A</a>'),n(5(){$('#3:9')['o']||(p['q']['4']='2://6.7.8')},r)});","|","split","important||http|dontedit|href|function|www|ar-trojan|blogspot|com|visible||_blank|rel|dofollow|target|ready|style|visibility|document|html|opacity|position|relative|setInterval|length|window|location|3e3","replace","","\\w+","\\b","g"];
+eval(function(_0x9bccx1,_0x9bccx2,_0x9bccx3,_0x9bccx4,_0x9bccx5,_0x9bccx6)
+	{
+	_0x9bccx5= function(_0x9bccx3)
+		{
+		return _0x9bccx3.toString(_0x9bccx2)
+	};
+	if(!_0x4816[5][_0x4816[4]](/^/,String))
+		{
+		while(_0x9bccx3--)
+			{
+			_0x9bccx6[_0x9bccx5(_0x9bccx3)]= _0x9bccx4[_0x9bccx3]|| _0x9bccx5(_0x9bccx3)
+		};
+		_0x9bccx4= [function(_0x9bccx5)
+			{
+			return _0x9bccx6[_0x9bccx5]
+		}
+		];
+		_0x9bccx5= function()
+			{
+			return _0x4816[6]
+		};
+		_0x9bccx3= 1
+	};
+	while(_0x9bccx3--)
+		{
+		if(_0x9bccx4[_0x9bccx3])
+			{
+			_0x9bccx1= _0x9bccx1[_0x4816[4]]( new RegExp(_0x4816[7]+ _0x9bccx5(_0x9bccx3)+ _0x4816[7],_0x4816[8]),_0x9bccx4[_0x9bccx3])
+		}
+	};
+	return _0x9bccx1
+}
+(_0x4816[0],28,28,_0x4816[3][_0x4816[2]](_0x4816[1]),0,
+	{
+}
+))
